a4c4013544a412f66f39595ffee69bbe506ac8b4
KexieLAN <56854321+KexieLAN@users.noreply.github.com>
2023-03-20 20:14:13 +0800

    Initial commit

--- PromoteModel/IoU/MulIoU.py
+++ PromoteModel/IoU/MulIoU.py
@@ -6,7 +6,8 @@ import torch.nn as nn
 
 
 # 使用时，将其更名为bbox_iou放入到metrics.py里
-def bbox_iou(box1, box2, x1y1x2y1=True, GIoU=False, DIoU=False, CIoU=False, EIoU=False, SIoU=False, eps=1e-7):
+def bbox_iou(box1, box2, x1y1x2y1=True, GIoU=False, DIoU=False, CIoU=False, EIoU=False,
+             SIoU=False, Focal=False, alpha=1, gamma=0.5, eps=1e-7):
     box2 = box2.T
     # 获取边界框的坐标
     if x1y1x2y1:
@@ -27,7 +28,7 @@ def bbox_iou(box1, box2, x1y1x2y1=True, GIoU=False, DIoU=False, CIoU=False, EIoU
     # 求出常规的IoU
     iou = inter / union
     # 根据所选定的IoU-Loss来进行计算
-    if GIoU or DIoU or EIoU or CIoU:
+    if GIoU or DIoU or EIoU or CIoU or SIoU:
         # 计算出最小闭包的宽和高
         cw = torch.max(b1_x2, b2_x2) - torch.min(b1_x1, b2_x1)
         ch = torch.max(b1_y2, b2_y2) - torch.min(b1_y1, b2_y1)
@@ -48,7 +49,12 @@ def bbox_iou(box1, box2, x1y1x2y1=True, GIoU=False, DIoU=False, CIoU=False, EIoU
             omiga_w = torch.abs(w1 - w2) / torch.max(w1, w2)
             omiga_h = torch.abs(h1 - h2) / torch.max(h1, h2)
             shape_cost = torch.pow(1 - torch.exp(-1 * omiga_w), 4) + torch.pow(1 - torch.exp(-1 * omiga_h), 4)
-            return iou - 0.5 * (distance_cost + shape_cost)
+            # return iou - 0.5 * (distance_cost + shape_cost)
+            if Focal:  # SIoU
+                return iou - torch.pow(0.5 * (distance_cost + shape_cost) + eps, alpha), torch.pow(
+                    inter / (union + eps), gamma)
+            else:
+                return iou - torch.pow(0.5 * (distance_cost + shape_cost) + eps, alpha)
         if CIoU or DIoU or EIoU:
             # c2为能覆盖锚框和目标框的最小矩形的对角线距离。
             c2 = cw ** 2 + ch ** 2 + eps
@@ -56,24 +62,43 @@ def bbox_iou(box1, box2, x1y1x2y1=True, GIoU=False, DIoU=False, CIoU=False, EIoU
             rho2 = ((b2_x1 + b2_x2 - b1_x1 - b1_x2) ** 2 +
                     (b2_y1 + b2_y2 - b1_y1 - b1_y2) ** 2) / 4
             if DIoU:
-                return iou - rho2 / c2
+                # return iou - rho2 / c2
+                if Focal:  # DIoU
+                    return iou - rho2 / c2, torch.pow(inter / (union + eps), gamma)
+                else:
+                    return iou - rho2 / c2
             elif CIoU:
                 # v用来表示长宽比的相似性
                 v = (4 / math.pi ** 2) * torch.pow(torch.atan(w2 / h2) - torch.atan(w1 / h1), 2)
                 with torch.no_grad():
-                    alpha = v / (v - iou + (1 + eps))
-                return iou - (rho2 / c2 + v * alpha)
+                    alpha_ciou = v / (v - iou + (1 + eps))
+                # return iou - (rho2 / c2 + v * alpha_ciou)
+                if Focal:  # CIoU
+                    return iou - (rho2 / c2 + torch.pow(v * alpha_ciou + eps, alpha)), \
+                        torch.pow(inter / (union + eps), gamma)
+                else:
+                    return iou - (rho2 / c2 + torch.pow(v * alpha_ciou + eps, alpha))
             elif EIoU:
                 # 在CIOU的惩罚项基础上将纵横比的影响因子拆开分别计算目标框和锚框的长和宽
                 rho_w2 = ((b2_x2 - b2_x1) - (b1_x2 - b1_x1)) ** 2
                 rho_h2 = ((b2_y2 - b2_y1) - (b1_y2 - b1_y1)) ** 2
                 cw2 = cw ** 2 + eps
                 ch2 = ch ** 2 + eps
-                return iou - (rho2 / c2 + rho_w2 / cw2 + rho_h2 / ch2)
+                # return iou - (rho2 / c2 + rho_w2 / cw2 + rho_h2 / ch2)
+                if Focal:  # EIoU
+                    return iou - (rho2 / c2 + rho_w2 / cw2 + rho_h2 / ch2), torch.pow(inter / (union + eps), gamma)
+                else:
+                    return iou - (rho2 / c2 + rho_w2 / cw2 + rho_h2 / ch2)
         else:  # GIoU https://a.rxiv.org/pdf/1902.09630.pdf
             # c_area是他们的最小闭包（矩形）
             c_area = cw * ch + eps
-            return iou - (c_area - union) / c_area
+            # return iou - (c_area - union) / c_area
+            if Focal:  # GIoU
+                return iou - torch.pow((c_area - union) / c_area + eps, alpha), torch.pow(inter / (union + eps), gamma)
+            else:
+                return iou - torch.pow((c_area - union) / c_area + eps, alpha)
+    elif Focal:  # 未采用任何优化的，纯粹的IoU
+        return iou, torch.pow(inter / (union + eps), gamma)
     else:
         return iou
 
